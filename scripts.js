487b61ffa45de6134a82422185f55f2fb28b816d
taniawaiganjo <taniawaiganjo@gmail.com>
2021-06-16 15:16:01 +0300

    merging branches

--- scripts.js
+++ scripts.js
@@ -5,7 +5,7 @@ function takevalue(){
     var year=document.getElementById("Year").value;
 }
 
-consle.log("date")
+
 
 var cc=year.charAt(0)+year.charAt(1)
 var yy=year.charAt(2)+year.charAt(3)
@@ -14,9 +14,9 @@ var yy=year.charAt(2)+year.charAt(3)
 
 var day= ( ( (cc/4) -2*cc-1) + ((5*yy/4) ) + ((26*(month+1)/10)) + date ) % 7
 
-consle.log("day")
 
-if (gender==="male") {
+
+if (gender == "male" && day == 1) {
 
 } 
 
