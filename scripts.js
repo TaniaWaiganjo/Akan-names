82340fee0c8e037d19fe0ab47c839657afa04580
taniawaiganjo <taniawaiganjo@gmail.com>
2021-06-16 13:54:51 +0300

    submit button

--- scripts.js
+++ scripts.js
@@ -1,7 +1,12 @@
+function takevalue(){
+    var gender=document.getElementById("Gender").value;
+    var date=document.getElementById("Date").value;
+    var month=document.getElementById("Month").value;
+    var year=document.getElementById("Year").value;
+}
 
 
-
-
+   
 
 
 
